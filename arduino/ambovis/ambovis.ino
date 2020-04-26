f63cc64075e6cb99de8d7eea4ea88b6093fb8c9a
luchouba <luciano.buglioni@gmail.com>
2020-04-26 16:15:36 -0300

    fixed release name in display

--- arduino/ambovis/ambovis.ino
+++ arduino/ambovis/ambovis.ino
@@ -291,7 +291,7 @@ void setup() {
   // Habilita el motor
   digitalWrite(PIN_EN, LOW);
 
-  writeLine(1, "AMBOVIS R_0_1",4);
+  writeLine(1, "AMBOVIS R_0_2",4);
 
   // configura la ventilación
   ventilation -> start();
