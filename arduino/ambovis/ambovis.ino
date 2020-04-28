55b830841eed0cfae212f7969822e6f9b78ce06d
luciano-nb <luciano.buglioni@gmail.com>
2020-04-27 22:24:56 -0300

    Release 3

--- arduino/ambovis/ambovis.ino
+++ arduino/ambovis/ambovis.ino
@@ -294,7 +294,7 @@ void setup() {
   // Habilita el motor
   digitalWrite(PIN_EN, LOW);
 
-  writeLine(1, "AMBOVIS 2704_v1",4);
+  writeLine(1, "AMBOVIS Release_0_3",4);
 
   // configura la ventilación
   ventilation -> start();
