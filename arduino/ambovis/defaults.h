d74bebde6fd65d77677a2319da13f762a7a84a0a
luchete80 <33092006+luchete80@users.noreply.github.com>
2020-06-01 13:46:21 -0300

    Update defaults.h

--- arduino/ambovis/defaults.h
+++ arduino/ambovis/defaults.h
@@ -43,7 +43,7 @@
 //#define STEPPER_LOWEST_POSITION     (STEPPER_MICROSTEPS *  -100)   // Steps
 //#define STEPPER_HIGHEST_POSITION    (STEPPER_MICROSTEPS *   100)   // Steps
 #define STEPPER_LOWEST_POSITION     (-10)   // Steps
-#define STEPPER_HIGHEST_POSITION    ( 2800)   //270º
+#define STEPPER_HIGHEST_POSITION    ( 2500)   //270º
 #define STEPPER_SPEED_DEFAULT       (400)   // Steps/s
 #define STEPPER_SPEED_MAX           (12000)   // Steps/s  //THIS IS FOR 1600 steps in a revolution. DO NOT GO BEYOND THIS!
 #define STEPPER_ACCEL_MAX           (12000)
