3f2d0e038a6c6474d34f51ddbd68dcb04abdd4cd
Mirian Quinteros <mn_quinteros@yahoo.com.ar>
2022-08-22 20:11:14 -0300

    added new mechanical ventilation functions

---
+++ arduino/ambovis/MechanicalVentilation.cpp
@@ -0,0 +1,107 @@
+#include "MechanicalVentilation.h"
+
+void initCycleTimes(MechanicalVentilation& mechanicalVentilation) {
+    VentilationStatus status = mechanicalVentilation.ventilationStatus;
+    VentilationConfig config = mechanicalVentilation.ventilationConfig;
+
+    status.timeoutCycle = ((float) 60) * 1000.0f / (float) config.respiratoryRate;
+    status.timeIns = status.timeoutCycle / (float(config.percIE + 1));
+    status.timeExp = status.timeoutCycle - status.timeIns;
+}
+
+void start(MechanicalVentilation& mechanicalVentilation) {
+    mechanicalVentilation.ventilationStatus.running = true;
+    initCycleTimes(mechanicalVentilation);
+}
+
+void stop(MechanicalVentilation& mechanicalVentilation) {
+    mechanicalVentilation.ventilationStatus.running = false;
+}
+
+void update(MechanicalVentilation& mechanicalVentilation, SensorData& sensorData) {
+
+    VentilationStatus status = mechanicalVentilation.ventilationStatus;
+
+    if ( !status.running ) {
+        return;
+    }
+
+    bool endedWhileMoving = false;
+    status.msTimerCnt = millis() - status.startCycleTimeInMs;
+  
+    float extra_time = 0.;
+    if (status.currentState == State_Exufflation) {
+        extra_time = status.timeIns;
+    }
+    status.cyclePosition = byte( (float) ( (status.msTimerCnt + extra_time) / (float) status.timeoutCycle * 127.0f) );
+
+    switch (status.currentState) {
+        case Init_Insufflation: {
+            float last_pressure_max = sensorData.max_pressure;
+            float last_pressure_min = sensorData.min_pressure;
+            sensorData.max_pressure = 0;
+            sensorData.min_pressure = 60;
+
+            status.startCycleTimeInMs = millis();
+        
+            status.cDynPass[0] = status.cDynPass[1];
+            status.cDynPass[1] = status.cDynPass[2];
+            status.cDynPass[2] = status.mlLastInsVol/(last_pressure_max - last_pressure_min);
+
+            status.cDyn = (status.cDynPass[0] + status.cDynPass[1] + status.cDynPass[2]) / 3.;
+            status.mlLastInsVol = int(sensorData.mlInsVol);
+            status.mlLastExpVol = int(fabs(sensorData.mlExsVol));
+        
+            sensorData.mlInsVol=0.;
+            sensorData.mlExsVol=0.;
+        
+            mechanicalVentilation.stepper->setSpeed(STEPPER_SPEED_MAX);
+            mechanicalVentilation.stepper->moveTo(-STEPPER_HIGHEST_POSITION);
+            mechanicalVentilation.stepper->setAcceleration(STEPPER_ACCEL_MAX);
+
+            status.currentState = State_Insufflation;
+            status.updateDisplay = true;
+
+        }
+        break;
+        case State_Insufflation: {
+            if (status.msTimerCnt > status.timeIns) {
+                if (mechanicalVentilation.stepper->distanceToGo() != 0 ) {
+                    endedWhileMoving = true;
+                }
+                status.currentState = Init_Exufflation;
+            }
+        }
+        break;
+        case Init_Exufflation: {
+            status.endedWhileMoving = endedWhileMoving;
+            status.startCycleTimeInMs = millis();
+
+            mechanicalVentilation.stepper->setAcceleration(STEPPER_ACCEL_MAX);
+            mechanicalVentilation.stepper->setSpeed(STEPPER_SPEED_EXSUFF);
+            mechanicalVentilation.stepper->moveTo(STEPPER_LOWEST_POSITION);
+            status.currentState = State_Exufflation;
+        }
+        break;
+        case State_Exufflation: {
+            if (status.msTimerCnt > status.timeExp) {
+                if (mechanicalVentilation.stepper->currentPosition() == STEPPER_LOWEST_POSITION) {
+                    status.currentState = Init_Insufflation;
+                    status.startCycleTimeInMs = millis();
+                    status.cycleNum += 1;
+                }
+            }
+        }
+        break;
+        case State_Homing: {
+             status.currentState = Init_Insufflation;
+        }
+        break;
+        case State_Error: break;
+    }
+
+}
+
+void update_config(MechanicalVentilation& mechanicalVentilation) {
+    initCycleTimes(mechanicalVentilation);
+}
---
+++ arduino/ambovis/MechanicalVentilation.h
@@ -0,0 +1,62 @@
+#ifndef MECHANICAL_VENTILATION_H
+#define MECHANICAL_VENTILATION_H
+
+#include "pinout.h"
+#include "defaults.h"
+#include "src/AccelStepper/AccelStepper.h"
+
+enum State {
+    Init_Insufflation = 1,
+    State_Insufflation = 2,
+    Init_Exufflation = 3,
+    State_Exufflation = 4,
+    State_Homing = 0,
+    State_Error = -1
+};
+
+typedef struct ventilation_config {
+    short respiratoryRate;
+    short peakInspiratoryPressure;
+    short peakExpiratoryPressure;
+    byte percIE;
+    byte percVolume;
+} VentilationConfig;
+
+typedef struct ventilation_status {
+    State currentState;
+    unsigned long msTimerCnt;
+    unsigned long cycleNum;
+    byte cyclePosition;
+    bool running;
+    float timeoutCycle;
+    unsigned int timeIns;
+    unsigned int timeExp;
+    unsigned long startCycleTimeInMs;
+    int cDynPass[3];
+    int mlLastInsVol;
+    int mlLastExpVol;
+    float cDyn;
+    bool endedWhileMoving;
+    bool updateDisplay;
+} VentilationStatus;
+
+typedef struct mechanical_ventilation {
+    AccelStepper * stepper = new AccelStepper(AccelStepper::DRIVER, PIN_STEPPER_STEP, PIN_STEPPER_DIRECTION);
+    VentilationConfig ventilationConfig;
+    VentilationStatus ventilationStatus;
+} MechanicalVentilation;
+
+typedef struct sensor_data {
+    int pressure_p;
+    int max_pressure;
+    int min_pressure;
+    float mlInsVol;
+    float mlExsVol;
+} SensorData;
+
+void start(MechanicalVentilation& mechanicalVentilation);
+void stop(MechanicalVentilation& mechanicalVentilation);
+void update(MechanicalVentilation& mechanicalVentilation, SensorData& sensorData);
+void update_config(MechanicalVentilation& mechanicalVentilation);
+
+#endif /* MECHANICAL_VENTILATION_H */
