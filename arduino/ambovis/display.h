3265d3c4884864d6b4aa76a3110f8d8ae26cf542
Mirian Quinteros <mn_quinteros@yahoo.com.ar>
2022-09-11 18:40:35 -0300

    fix branch merge unused change

--- arduino/ambovis/display.h
+++ arduino/ambovis/display.h
@@ -7,8 +7,6 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
 
-//SPI HARDWARE SHOULD BE PINS CLK #13 AND MISO #11, WITH THIS CONSTRUCTOR!
-void drawY2(uint16_t color);
 void print_bat(Adafruit_ILI9341& tft, float fac);
 void print_float(Adafruit_ILI9341& tft, const int &row, const int &col, const float &val);
 float calc_bat(const int &iter, float fac);
