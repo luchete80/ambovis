6c353a26761644e761896ef30497a851559357cd
mnquinteros <mn_quinteros@yahoo.com.ar>
2023-06-25 11:15:44 -0300

    add python script for deployment

--- src/ambovis/private_data.h
+++ src/ambovis/private_data.h
@@ -1,7 +1,5 @@
-
 #ifndef AMBOVIS_PRIVATE_DATA_H
-#define AMBOVIS_PRIVATE_DATA_H  
+#define AMBOVIS_PRIVATE_DATA_H
 static float dp[] = {0., 0., 0., 0., 0., 0., 0., 0., 0., 0.};
 static byte po_flux[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 #endif
-
