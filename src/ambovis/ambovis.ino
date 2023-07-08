fb9a31de932a199053d9154a466fdbb7a62e2e3d
mnquinteros <mn_quinteros@yahoo.com.ar>
2023-07-08 17:52:24 -0300

    change version

--- src/ambovis/ambovis.ino
+++ src/ambovis/ambovis.ino
@@ -75,7 +75,7 @@ void setup() {
     init_display_lcd();
 
     writeLine(1, "RespirAR FIUBA", 4);
-    writeLine(2, "v2.0.4", 8);
+    writeLine(2, "v2.1.1", 8);
 
     init_display_tft(tft);
     init_sensor(ads);
